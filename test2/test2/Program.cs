2ed0f6817237b3c5ba4e90a1cc98a47a004b61a8
Opilane TTHK <118337312+MrMustela@users.noreply.github.com>
2022-11-16 11:24:11 +0200

    test

---
+++ test2/test2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite number. 1 to 100.");
+            string number = Console.ReadLine();
+
+            int numberParsed = Int16.Parse(number);
+
+            // saab ka panna Console.WriteLine(Convert.ToInt(number));
+
+
+            // if ja else'i lisamine
+            // kui alla 50, siis tuleb vastus. 
+
+            Console.ReadKey();
+
+            if ( numberParsed >= 1 && numberParsed <= 100)
+
+            {
+                Console.WriteLine("Your favorite number is " + number );
+
+
+            }
+            else if ( numberParsed <= 0 && numberParsed > 101 ) 
+            {
+                Console.WriteLine("ERROR! Only 1 to 100 numbers are allowed!");
+            }
+            
+        
+        }
+    }
+}
