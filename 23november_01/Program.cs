3752ffa231a27f82288c59ee5a07aacfec2ec0a2
MrMustela <118337312+MrMustela@users.noreply.github.com>
2022-11-23 11:23:03 +0200

    Add files via upload

---
+++ 23november_01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _23november_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta nimi");
+            string nimi = Console.ReadLine();
+
+            Console.WriteLine("Tere, " + nimi + ".");
+            
+
+            Console.WriteLine("Mis on sinu vanus?");
+            
+            string vanus = Console.ReadLine();
+            int firstvanus = Convert.ToInt16(vanus);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+   
+            Console.WriteLine("Sinu vanus on " + firstvanus + ", " + nimi + ".");
+                
+
+
+            
+            
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
