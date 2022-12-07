0cca6fcd54570cf2ccecf3e7f6b645eff7cb6620
MrMustela <118337312+MrMustela@users.noreply.github.com>
2022-12-07 10:24:46 +0200

    07.12.22
    tunnitöö - for loop ja flowcharti kasutamine.

---
+++ 7detsember_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _7detsember_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int i = 0;
+
+            for (;;)
+            {
+                if (i < 10)
+                {
+                    Console.WriteLine("i väärtus on: {0}", i);
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
