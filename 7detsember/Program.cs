d1611817c31471b5f3ff78b5677f47a613fd12c3
MrMustela <118337312+MrMustela@users.noreply.github.com>
2022-12-07 09:09:36 +0200

    Add files via upload

---
+++ 7detsember/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _7detsember
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ushort i = 0;
+            // hakkab itereerima nii kaua kuni ushort
+            // andmetüübist saadakse kätte maksimaalne arv e 63 tuhat
+            // peale seda alustab uuesti
+            // lõputu itereeimine tuleneb tingimuste puudumisest
+            for (;;)
+            {
+                Console.WriteLine("i väärtus on: {0}", i );
+                i++;
+            }
+            
+        }
+    }
+}
