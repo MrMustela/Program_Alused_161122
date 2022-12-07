d2c964c71041b302fe9490a131c7e377604a905e
MrMustela <118337312+MrMustela@users.noreply.github.com>
2022-12-07 10:45:57 +0200

    07.12.22
    püramiid

---
+++ 7detsember_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+
+namespace _7detsember_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int i, j, rows;
+            
+            Console.WriteLine("Sisesta ridade arv");
+            
+                rows = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+
+       
+    }
+}
