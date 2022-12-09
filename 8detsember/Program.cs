da19a319f3768de83aeafc08a4094ab6feee3888
MrMustela <118337312+MrMustela@users.noreply.github.com>
2022-12-09 12:40:39 +0200

    9 detsember not 8 detsember, Today is not my day.
    lol!

---
+++ 8detsember/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _8detsember
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, sum = 0;
+            
+            Console.WriteLine("Display the sum on odd number: ");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("Sisesta number");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("The usual number are: ");
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("{0} ", 2*i-2);
+                sum += 2 * i - 2;
+            }
+            Console.Write(" The sum on odd natural number up to {0} terms: {1} \n ", n, sum);
+        }
+    }
+}
