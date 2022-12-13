d8db0c15681301abfe79f611e31e7ae7ee269ec7
MrMustela <118337312+MrMustela@users.noreply.github.com>
2022-12-13 08:53:42 +0200

    13.12.22
    Tunnitöö - for loopi jätkamine. 
    Teemaks: püramiid

---
+++ 13detsember/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _13detsember
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int i, j, rows;
+            
+            Console.WriteLine("Sisesta ridade number");
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                   
+                    Console.Write(i);
+               
+                }
+                Console.Write("\n");
+            }
+
+            
+        }
+    }
+}
