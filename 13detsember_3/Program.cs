07081a6ac19a59029dd9458fbe66adfb17da81f7
MrMustela <118337312+MrMustela@users.noreply.github.com>
2022-12-13 10:31:56 +0200

    13.12.22
    Tunnitöö - A-täht

---
+++ 13detsember_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _13detsember_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A-täht");
+
+            int row, column;
+
+            for (row = 0; row <= 7; row++)
+            {
+                for (column = 0; column <= 7; column++)
+                {
+                    if (((column == 1 || column == 5) && row != 0) ||
+                       ((row == 0 || row == 3) && (column > 1 && column < 5)))
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    
+                    
+                }
+                Console.Write("\n");
+            
+            }
+            Console.Write("\n");
+        }
+    }
+}
