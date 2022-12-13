20b99fdb85ea901fc3d568b829850385cb02fda3
MrMustela <118337312+MrMustela@users.noreply.github.com>
2022-12-13 09:14:00 +0200

    13.12.22
    tunnitöö - püramiid. for loopi kasutamine.

---
+++ 13detsember_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _13detsember_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, spc, rows, k, t = 1;
+
+            Console.WriteLine("Numbri püramiid");
+                
+            Console.WriteLine("Sisesta ridade arv");
+
+            rows = Convert.ToInt32(Console.ReadLine());
+            spc = rows + 4 - 1;
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (k = spc; k >= 1; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("{0} ", t++);
+                }
+                Console.Write("\n");
+                spc--;
+            }
+
+        }
+    }
+}
