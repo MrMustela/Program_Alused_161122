ac907802ce6a1f89cc8e573e60e2696f2981ded8
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-10 09:56:02 +0200

    Beep,
    10.01.2023

---
+++ 10jaanuar_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _10jaanuar_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number ja vajuta enter");
+
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("One");
+                    Console.Beep();
+                    break;
+
+                case 2:
+                    Console.WriteLine("Two");
+                    Console.Beep();
+                    Console.Beep();
+                    break ;
+
+                case 3:
+                    Console.WriteLine("Three");
+                    Console.Beep();
+                    Console.Beep();
+                    Console.Beep();
+                    break;
+
+                default:
+                    Console.WriteLine("Some other number");
+                    break;
+            }
+        }
+    }
+}
