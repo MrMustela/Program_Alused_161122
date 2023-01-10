f8cf3667119b01dc68c4354a16de0e380d8f92a0
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-10 10:43:53 +0200

    Värvid
    10.01.2023

---
+++ 11jaanuar_4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _11jaanuar_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suvalised värvid");
+
+            Color c = (Color)new Random().Next(0, 5);
+
+            switch (c)
+            {
+                case Color.Red:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Clear();
+                    Console.WriteLine("Punane");
+                    break;
+
+                case Color.Green:
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.Clear();
+                    Console.WriteLine("Roheline");
+                    break;
+
+                case Color.Blue:
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.Clear();
+                    Console.WriteLine("Sinine");
+                    break;
+
+                case Color.White:
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    Console.WriteLine("Valge");
+                    break;
+
+                case Color.Yellow:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.Clear();
+                    Console.WriteLine("Kollane");
+                    break;
+
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
+
+
+
+
+        public enum Color
+        {
+            Red,
+            Green,
+            Blue,
+            White,
+            Yellow    
+        }
+    }
+}
