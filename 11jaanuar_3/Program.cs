fee0b87bfb43ad04ce66b85fc278065f70514a47
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-10 10:25:33 +0200

    Kuupäev
    10.01.2023

---
+++ 11jaanuar_3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace _11jaanuar_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start:
+
+            Console.WriteLine("Hello World!");
+
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("Täna on pühapäev");
+                    break;
+
+                    case DayOfWeek.Monday:
+                    Console.WriteLine("Täna on esmaspäev");
+                    break;
+
+                    case DayOfWeek.Tuesday:
+                    Console.WriteLine("Nüüd");
+                    break;
+
+                    case DayOfWeek.Wednesday:
+                    Console.WriteLine("Täna on kolmapäev");
+                    break;
+
+                    case DayOfWeek.Thursday:
+                    Console.WriteLine("Täna on neljapäev");
+                    break;
+
+                    case DayOfWeek.Friday:
+                    Console.WriteLine("Täna on reede");
+                    break;
+
+                    case DayOfWeek.Saturday:
+                    Console.WriteLine("Täna on laupäev");
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+
+            }
+            Console.ReadLine();
+            //goto Start;
+        }
+    }
+}
