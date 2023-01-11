fa31dab102a4e94d391b29467140c8daebac1cef
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-11 10:13:21 +0200

    Switchi harjutus
    11.01.2023

---
+++ real11jaanuar/real11jaanuar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace real11jaanuar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palun sisesta number 1 kuni 3.");
+           
+            double greeting = Convert.ToDouble(Console.ReadLine()); 
+            double greeting2 = Convert.ToDouble(greeting);
+            //float greeting = 1.2F;
+            switch (greeting2)
+            {
+                case 1:
+                    Console.WriteLine("Hello");
+                    goto case 2;
+                    
+                case 2:
+                    Console.WriteLine("Bonjour");
+                    goto case 3;
+
+                case 3:
+                    Console.WriteLine("Namaste");
+                    goto default;
+
+                default:
+                    Console.WriteLine("Entered value is: " + greeting2);
+                    break;
+            }
+        }
+    }
+}
