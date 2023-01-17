52beecab1e0352e86bc97875888905a31c2b5eec
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-17 09:20:52 +0200

    17.jaanuar - switch
    Tunnitöö - switchi kasutamine

---
+++ 17jaanuar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _17jaanuar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                Console.WriteLine("This is number " + number);
+                break;
+
+                case 2:
+                Console.WriteLine("This is number " + number);
+                break;
+
+                case 3:
+                Console.WriteLine("This is number " + number);
+                break;
+
+                default:
+                Console.WriteLine("Error");
+                break;
+
+            }
+            
+        
+        }
+
+           
+    }
+}
