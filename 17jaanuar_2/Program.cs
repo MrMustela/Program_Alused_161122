ba71a78e77df27eec2e2d221051688215ce45584
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-17 10:00:40 +0200

    17.01.23 - switch

---
+++ 17jaanuar_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _17jaanuar_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta hinne");
+
+            string mark = Console.ReadLine();
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            switch (mark)
+            {
+                case "A":
+                
+                    Console.WriteLine("Suurepärane töö!");
+                    break;
+
+                case "B":
+                case "C":
+                    Console.WriteLine("Hea töö");
+                    break;
+
+                case "D":
+                    Console.WriteLine("Said läbi");
+                    break;
+
+                case "F":
+                    Console.WriteLine("Failer oled");
+                    break;
+
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
+    }
+}
