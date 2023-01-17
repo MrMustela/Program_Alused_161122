6ff43502f4ab7ca10f15dc09b01c3dc8eb1a1640
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-17 10:47:49 +0200

    17.01.23 - Switch #4

---
+++ 17jaanuar_4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace _17jaanuar_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Te saate sisestada 4 asja, kas sisestad if.else, ternary, switch või üllatus!");
+
+            string text = Console.ReadLine();
+            
+            switch (text)
+            {
+                case "if.else":
+                Console.WriteLine("See on " + text);
+                    break;
+
+                case "ternary":
+                    Console.WriteLine("See on " + text);
+                    break;
+
+                case "switch":
+                    Console.WriteLine("see on " + text);
+                    break;
+
+                case "üllatus":
+                    Console.WriteLine("See ongi " + text);
+                    break;
+
+                default:
+                    Console.WriteLine("Error");
+                    break;
+
+            }
+
+                
+
+
+        }
+    }
+}
