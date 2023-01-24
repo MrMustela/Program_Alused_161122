416388a0db5aaa96c8d2a7b0ec0a67f09a68d880
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-24 10:18:47 +0200

    24.01.23 - While_4

---
+++ 24jaanuar_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _24jaanuar_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do While loop");
+
+            int i = 1, n = 5, result;
+
+            do
+            {
+                result = n * i;
+                Console.WriteLine("{0} * {1} = {2}", n, i, result);
+                i++;
+            } while (i <= 10);
+        }
+    }
+}
