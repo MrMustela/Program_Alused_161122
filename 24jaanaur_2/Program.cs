e6b917678d82d755cc826cebe7d289abd2f0a51d
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-24 09:38:25 +0200

    24.01.23 - while, praktika

---
+++ 24jaanaur_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace _24jaanaur_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While2");
+
+            int i = 0;
+
+            while(true)
+            {
+                Console.WriteLine("i = {0}", i);
+                i++;
+
+                if (i > 10)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
