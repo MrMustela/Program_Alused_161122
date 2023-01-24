48043154c9d53b6ed667660ab1085ea9eeba6949
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-24 08:48:39 +0200

    24.jaanuar - while

---
+++ 24jaanuar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _24jaanuar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("24.jaanuar");
+
+            int i = 1;
+
+            while (i <= 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
