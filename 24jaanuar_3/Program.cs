945e606f8588c2aed2159e73c99efdf49f1674f6
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-24 10:05:18 +0200

    24.01.23 - while_3

---
+++ 24jaanuar_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _24jaanuar_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While 3");
+
+            int i = 0, j = 1;
+
+            while (i < 2)
+            {
+                Console.WriteLine("i = {0}", i);
+                i++;
+
+                while (j <= 2)
+                {
+                    Console.WriteLine("j = {0}", j);
+                    j++;
+                }
+            }
+        }
+    }
+}
