8e9811a06cd613c7aa0e5e05db0952bd4d64a89e
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-25 09:20:25 +0200

    25.01.23 - kujundid

---
+++ 25jaanuar_2/25jaanuar_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _25jaanuar_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number: ");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+
+            while (i <= n)
+            {
+                Console.WriteLine();
+                int j = 1;
+                while (j <= n)
+                {
+                    Console.Write(j + " ");
+                    j++;
+                }
+                i++;
+            }
+           
+
+            Console.WriteLine("\n\n\nKolmnurk ");
+
+            n = Convert.ToInt32(Console.ReadLine());
+            i = 1;
+
+            while (i <= n)
+            {
+                Console.WriteLine();
+                int j = 1;
+                while (j <= i)
+                {
+                    Console.Write(j + " ");
+                    j++;
+                }
+                i++;
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
