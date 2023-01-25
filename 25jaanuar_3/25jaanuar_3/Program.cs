31866e544671a0a2ddbe1373a9c170cb192a36ba
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-25 10:06:41 +0200

    25.01.23 - ruut

---
+++ 25jaanuar_3/25jaanuar_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _25jaanuar_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int a = 1;
+            while (a <= 5)
+            {
+                int b = 1;
+                while (b <= 5)
+                {
+                    Console.Write(b + " ");
+                    b++;
+                }
+                Console.WriteLine();
+                a++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
