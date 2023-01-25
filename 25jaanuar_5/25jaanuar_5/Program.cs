cfc64ca76e7610683bbf8b6775ace6cdea8b56b1
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-25 10:44:21 +0200

    25.01.23 - kodutöö
    Tähtaeg: 31.01.23

---
+++ 25jaanuar_5/25jaanuar_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _25jaanuar_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While kodutöö, sellest programmist teha vooskeem");
+            Console.WriteLine("NB! Vooskeem teha .html faililaiendina ja saata emaili.");
+            int number = 1;
+
+            do
+            {
+                Console.Write($"{number} ");
+                number++;
+            } while (number <= 5);
+        }
+    }
+}
