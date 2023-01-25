6921975dd5998d952a8f5976875d58f11ba91945
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-25 08:55:05 +0200

    25.01.23 - Fibonacci jada

---
+++ 25jaanuar/25jaanuar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace _25jaanuar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+
+            int i, n, j, k;
+            
+            n = Convert.ToInt32(Console.ReadLine());
+
+            i = 0;
+            j = 1;
+            Console.Write(i + " " + j);
+
+            k = i + j;
+            while (k <= n)
+            {
+                Console.Write($" {k}");
+
+                i = j;
+                j = k;
+                k = i + j;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
