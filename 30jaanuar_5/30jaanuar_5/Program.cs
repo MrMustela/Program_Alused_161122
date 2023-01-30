def5b2330a6da6a85f2193b60b3574a3804463eb
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-30 14:26:24 +0200

    30.01.23 - hello

---
+++ 30jaanuar_5/30jaanuar_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _30jaanuar_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] myArray = { 'H', 'e', 'l', 'l', 'o' };
+            
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
