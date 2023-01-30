23973d1523024c6face4c665dd8aacb1d6fb1959
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-30 14:56:43 +0200

    30.01.23 - genders

---
+++ 30jaanuar_6/30jaanuar_6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _30jaanuar_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array 6");
+
+            string[] genders = {"m", "f", "m", "m", "m", "f"
+            , "f", "f", "m", "m", "f"};
+
+            int female = 0, male = 0;
+
+            foreach (var gender in genders)
+            {
+                if (gender == "f")
+                {
+                    female++;
+                }
+                else
+                {
+                    male++;
+                }
+                // teine viis, seda nimetatakse lühikeseks if'ks ja else-ks
+                // var result = gender == "f" ? female++ : male++;
+            }
+
+            Console.WriteLine("Nr of female {0}", female);
+            Console.WriteLine("Nr of male {0}", male);
+        }
+    }
+}
