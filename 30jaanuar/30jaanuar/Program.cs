5ad612c638e90559d3e61fe0c3c28375505cf95b
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-30 13:02:07 +0200

    30.01.2023 - Array e jada e massiiv

---
+++ 30jaanuar/30jaanuar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _30jaanuar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array e jada e massiiv");
+
+            //loome massiivi
+            int[] array = new int[10];
+
+            array[0] = 10; //massiivi kasutamine
+            array[2] = 20;
+            array[4] = 30;
+
+            //massiiv käib kõik numbrid läbi
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
