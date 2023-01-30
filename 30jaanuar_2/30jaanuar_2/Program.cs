720ffe2aa142575ef5303a23eb14889f286317a3
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-30 13:12:57 +0200

    30.01.2023 - Array selgitus.

---
+++ 30jaanuar_2/30jaanuar_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _30jaanuar_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array");
+
+            //muutuja nimega array sisse paneme mitu
+            //väärtust numbritega 10, 20, 30, 40 ja 50
+            //mitu muutujat saab panna, kui
+            //andmetüübi järele paned märgid []
+
+            int[] array = { 10, 20, 30, 40, 50 };
+            
+            //siin kuvatakse kõik need numbrid 
+            //kindlasti peab panema muutuja järele .Length
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
