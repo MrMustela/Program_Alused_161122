2ee88b7fe616896a7c0cd913107f1e37aa9d29eb
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-30 13:36:19 +0200

    30.01.23 - array meelde jätmine.

---
+++ 30jaanuar_3/30jaanuar_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _30jaanuar_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array 3");
+
+            int[] numbers = { 22, 9, 19 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
