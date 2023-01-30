edb708f31d92213e4dcc8bd49dcfab0d2ff16254
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-30 13:49:53 +0200

    30.01.2023 - elemendid kasutades array-t.

---
+++ 30jaanuar_4/30jaanuar_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _30jaanuar_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array 4");
+            
+            int[] array = new int[10];
+            int i;
+
+            Console.Write("Loe ja prindi massiiv");
+
+            Console.WriteLine("Kokku on 10 elementi");
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("element - {0} :", i);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Elemendid massiivis on: ");
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            Console.Write("\n");
+        }
+    }
+}
