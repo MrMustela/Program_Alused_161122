51071464f13f490a12a0a94d96d8d498c13d2d7e
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-31 09:06:11 +0200

    31.01.2023 - Pealinnad Euroopas

---
+++ 31jaanuar/31jaanuar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _31jaanuar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+            string sentence = "Euroopa üks pealinnadest on: ";
+
+            var cities = Tuple.Create("Tallinn", "Riga", "Vilnius",
+                "Helsinki", "Stockholm", "Pariis", "Oslo", "Berliin");
+            Console.WriteLine(sentence + cities.Item1);
+            Console.WriteLine("-------------------------------");
+            
+            string[] city = {"Tallinn", "Riga", "Vilnius",
+                "Helsinki", "Stockholm", "Pariis", "Oslo", "Berliin" };
+            for (int i = 0; i < city.Length; i++)
+            {
+                Console.WriteLine(sentence + city[i]);
+            }
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine(sentence + city[2]);
+            Console.WriteLine("-------------------------------");
+
+            // muutuja nimega number on nüüd suvaline number
+            Random number = new Random();
+            // muutuja randomNumber annab vahemikus 0 kuni 4 väärtusega tulemusi
+            int randomNumber = number.Next(0, 4);
+            // nüüd saame tulemusi suvaliselt 
+            Console.WriteLine(sentence + city[randomNumber]);
+        }
+    }
+}
