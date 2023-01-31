24e0f46af28cba7240283f3ec9a69e7405251957
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-31 10:40:59 +0200

    31.01.2023 - järjestamine

---
+++ 31jaanuar_5/31jaanuar_5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _31jaanuar_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] animals = { "cat", "alligator", "fox", "donkey", "bear", "elephant", "goat", "rabbit", "chinchilla", "rat", "giraffe", "zebra", "mouse", "dog" };
+
+            //Array.Sort(animals);
+
+            //foreach (var animal in animals)
+            //{
+            //    Console.WriteLine(animal);
+            //}
+            Console.WriteLine("--------------------------");
+            //järjestab kolm esimest sõna (elementi)
+            //tähestikulises järjestuses
+            Array.Sort(animals, 0, 3);
+
+            foreach (string animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+
+            Console.WriteLine("------------------------");
+            int[] numbers = { 1, 2, 3, 4, 3, 55, 23, 2 };
+            Array.Sort(numbers);
+            //funktsioon nimega distinct välistab kordused
+            int[] distinct = numbers.Distinct().ToArray();
+
+            foreach (var dist in distinct)
+            {
+                Console.WriteLine(dist);
+            }
+
+
+
+        }
+    }
+}
