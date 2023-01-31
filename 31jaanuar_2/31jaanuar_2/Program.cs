42a9f68aeefc0b68ab3e4db1ccb2fbaf36886916
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-31 09:38:48 +0200

    31.01.2023 - array

---
+++ 31jaanuar_2/31jaanuar_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _31jaanuar_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int stringRowMin = 0;
+            int stringRowMax = 5;
+
+            int[] randomAsterisk = new int[stringRowMax];
+
+            // esimene viis
+            Random asterisk = new Random();
+            for (int i = stringRowMin; i < randomAsterisk.Length; i++)
+            {
+                Console.WriteLine(string.Concat(Enumerable.Repeat("*",
+                asterisk.Next(1, 20))));
+            }
+
+            Console.WriteLine("------------------------------------");
+            // teine viis
+            foreach (var item in randomAsterisk)
+            {
+                Console.WriteLine(string.Concat(Enumerable.Repeat("*",
+                asterisk.Next(1, 20))));
+            }
+            
+
+            
+        }
+    }
+}
