cc7c81afc747a3c3b016d94f2a20efbd57371f9e
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-01-31 10:11:32 +0200

    31.01.2023 - Array viisid

---
+++ 31jaanuar_4/31jaanuar_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _31jaanuar_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 15, 16, 8, 6 };
+            
+            Console.WriteLine("---------------");
+            Console.WriteLine("õige järjestusega massiiv");
+            foreach (var element in numbers)
+            {
+                Console.WriteLine(element);
+            }
+           
+            Console.WriteLine("---------------");
+            Console.WriteLine("Alates väiksemast kuni suuremani");
+            Array.Sort(numbers);
+
+            foreach (var element in numbers)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("---------------");
+            Array.Reverse(numbers);
+            Console.WriteLine("tagurpidi numbrite jada");
+            Array.ForEach<int>(numbers, n => Console.WriteLine(n));
+
+            Console.WriteLine(Array.BinarySearch(numbers, 15));
+        }
+    }
+}
