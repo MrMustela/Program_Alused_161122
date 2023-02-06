901b27c7be4ab0111e816d180e2326dccd04e001
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-02-06 14:30:08 +0200

    Add files via upload

---
+++ 6veebruar/6veebruar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6veebruar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
