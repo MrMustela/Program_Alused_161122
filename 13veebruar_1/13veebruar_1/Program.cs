6414409fecca2fdbe3a599509dd7f55644365845
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-02-13 14:52:00 +0200

    13.02.23 - foreach-i jätkamine.

---
+++ 13veebruar_1/13veebruar_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _13veebruar_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new List<List<string>>
+           {
+               new List<string> {"tea", "road", "car", "sky"},
+               new List<string> {"cup", "falcon", "loan", "airplane"},
+               new List<string> {"head", "snow", "frost", "fish"}
+
+           };
+
+            foreach (var nested in words)
+            {
+                foreach (var word in nested)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
