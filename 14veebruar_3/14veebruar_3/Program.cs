ce3b92e7b6ff1ae32a73fa0b30107a4ee76b1b2d
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-02-14 10:31:48 +0200

    14.02.2023 - Foreach-i jätkamine
    Praktiline töö - states

---
+++ 14veebruar_3/14veebruar_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14veebruar_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var states = new Dictionary<string, string>();
+
+            states.Add("Estonia", "EST");
+            states.Add("Latvia", "LAT");
+            states.Add("Finland", "FIN");
+            states.Add("Sweden", "SWE");
+
+            foreach (var state in states)
+            {
+                Console.WriteLine($"{state.Key} - {state.Value}");
+            }
+            Console.WriteLine("------------------------------");
+            Console.WriteLine(states["Estonia"]);
+            Console.WriteLine(states["Latvia"]);
+            Console.WriteLine(states["Finland"]);
+            Console.WriteLine("------------------------------");
+            if (states.ContainsKey("Estonia"))
+            {
+                Console.WriteLine(states["Estonia"]);
+            }
+            else
+            {
+                Console.WriteLine("Nimega Estonia ei eksisteeri");
+            }
+        }
+    }
+}
