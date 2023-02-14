6978c68fcf7c3631751f92244bc7fbb83cdea581
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-02-14 09:10:52 +0200

    14.02.23 - foreach
    Foreachi erinevalt kasutamine.

---
+++ 14veebruar/14veebruar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _14veebruar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var domain = new Dictionary<string, string>()
+            {
+                {"est", "Estonia"},
+                {"lat", "Latvia"},
+                {"fin", "Finland"},
+                {"swe", "Sweden"},
+            };
+            
+            //esimene lihtne variant
+            foreach (var pair in domain)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value}");
+            }
+            
+            //on ka teine viis, kuid raskem.
+            //domain.ToList().ForEach(x => Console.WriteLine($"{x.Key} - {x.Value}")); 
+        }
+    }
+}
