1b6d1a28a59c8175fff9a4b380d9aff5b66f2931
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-02-14 09:41:15 +0200

    14.02.23 - ForReachi jätkamine
    praktika

---
+++ 14veebruar_2/14veebruar_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14veebruar_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Dictionary<int, string>
+            {
+                {1, "Estonia"},
+                {2, "Latvia"},
+                {3, "Lithuania"},
+            };
+            
+            //result.ToList().ForEach(x => Console.WriteLine($"{x.Key} - {x.Value}"));
+
+            foreach (var pair in result)
+            {
+               Console.WriteLine($"{pair.Key} - {pair.Value}");
+            }
+        }
+    }
+}
