4d38ddbc0b40b96d26d8c796aaadb1f994fa5ecd
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-02-14 10:41:01 +0200

    14.02.23 - ForReach-i jätkamine
    Praktiline töö - Linnad ja maakonnad

---
+++ 14veebruar_4/14veebruar_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14veebruar_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cities = new Dictionary<string, string>()
+            {
+                {"Harju", "Tallinn" },
+                {"Läänemaa", "Haapsalu" },
+                {"Raplamaa", "Rapla" }
+            };
+
+            cities.Remove("Harju");
+
+            if (cities.ContainsKey("Läänemaa"))
+            {
+                cities.Remove("Läänemaa");
+            }
+
+            Console.WriteLine("Total elements: {0}", cities.Count);
+        }
+    }
+}
