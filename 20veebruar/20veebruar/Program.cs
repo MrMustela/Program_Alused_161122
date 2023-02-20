dec92e820609b421e8dc146f3fdedb0a4982641f
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-02-20 14:06:34 +0200

    20.02.23 - foreach ja dictionary
    Tänavad

---
+++ 20veebruar/20veebruar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _20veebruar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var street = new Dictionary<string, string>
+            {
+                {"Ristiku", "tänav" },
+                {"Tuulemaa", "tänav" },
+                {"Puhangu", "tänav" }
+            };
+
+            foreach (var paar in street)
+            {
+                Console.WriteLine($"{paar.Key} {paar.Value}");
+            }
+
+        }
+    }
+}
