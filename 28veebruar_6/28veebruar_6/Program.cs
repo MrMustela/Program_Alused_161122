fb759f95d5adce60cd9c60b651146278395f19f9
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-02-28 10:41:33 +0200

    enum - maja tüübid
    28.02.23

---
+++ 28veebruar_6/28veebruar_6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _28veebruar_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enum");
+            // teha enum majatüüpidest ja kutsuda neid Main meetodis esile
+
+            Console.WriteLine();
+            //näitab muutuja vastust
+            Console.WriteLine(houseTypes.Singlefamily);
+            Console.WriteLine("-------------------------------------");
+            //mingi antud muutuja väärtus näitab kindla muutuja vastust
+            var ht = (houseTypes)1;
+            Console.WriteLine(ht);
+            Console.WriteLine("-------------------------------------");
+            //muutuja muutub teatud tüüpi väärtuseks
+            int house = (int)houseTypes.Multifamily;
+            Console.WriteLine(house);
+        }
+
+        enum houseTypes
+        {
+            Singlefamily,
+            Townhouse,
+            Multifamily,
+            Modularhome,
+            Penthouse // = 1
+
+        }
+    }
+}
