8c18ceb371e801f4b1aae9cb9145e004b994c9e7
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-02-28 08:47:45 +0200

    Struct

---
+++ 28veebruar/28veebruar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _28veebruar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Struct");
+
+            //meetodi välja kutsumine
+            Coordinate coordinate = new Coordinate();
+
+            Console.WriteLine(coordinate.x);
+            Console.WriteLine(coordinate.y);
+
+        }
+    }
+
+    struct Coordinate
+    {
+        public int y;
+        public int x;
+    }
+}
