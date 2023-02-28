a68e0046bbc292dad227150da6382fce97e27719
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-02-28 10:05:41 +0200

    Enum - nädalapäevad
    28.02.23

---
+++ 28veebruar_5/28veebruar_5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _28veebruar_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enum");
+
+            //enumi esile kutsumine
+            Console.WriteLine(Weekdays.Friday);
+
+            //saame teada, mis numbrilise väärtusega on reede ja vastus on 4
+            int day = (int)Weekdays.Monday;
+            Console.WriteLine(day);
+
+            //teine variant, et kuidas saada näidata päeva nimetust
+            var wd = (Weekdays)5;
+            Console.WriteLine(wd);
+        }
+
+
+        enum Weekdays
+        {
+            //enum on numereerija
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday,
+
+        }
+    }
+}
