bbea700c259279a8ef8816f036cb3d88864f4ca0
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-02-28 09:56:52 +0200

    Struct - cars
    28.02.23

---
+++ 28veebruar_4/28veebruar_4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _28veebruar_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Struct - cars");
+
+            //Structi välja kutsumine
+            Cars cars = new Cars();
+            //Structi sees oleva meetodi välja kutsumine
+            cars.Engine();
+
+            Console.WriteLine(cars.x);
+            Console.WriteLine(cars.y);
+
+            Cars cars2 = Cars.Getengines();
+            //Kõik väärtused on nullid, sest nendele pole väärtus määratud
+            Console.WriteLine(cars2.x);
+            Console.WriteLine(cars2.y);
+
+        }
+
+        struct Cars
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+
+            //Structi sisse tuleb meetod
+            public void Engine()
+            {
+                x = 5;
+                y = 10;
+            }
+            //Structis on staatiline meetod
+            public static Cars Getengines()
+            {
+                return new Cars();
+            }
+        }
+    }
+}
