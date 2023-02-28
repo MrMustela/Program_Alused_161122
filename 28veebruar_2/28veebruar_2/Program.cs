d7d1499e1f922e1014ddb6ba6a844ebf9250f0e2
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-02-28 09:02:50 +0200

    Struct Employee
    28.02.23 - väärtuse andmine ja class-i kasutamine.

---
+++ 28veebruar_2/28veebruar_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _28veebruar_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Struct");
+            
+            //Employee emp;
+            //Employee employee2 = new();
+            Employee employee = new Employee();
+
+            Employee emp;
+            emp.Id = 5;
+            Console.WriteLine(emp.Id);
+
+            Employee empF;
+            empF.FirstName = "Peeter";
+            Console.WriteLine(empF.FirstName);
+
+            
+            
+            //Console.WriteLine(employee.Id);
+            //Console.WriteLine(employee.FirstName);
+            //Console.WriteLine(employee.LastName);
+            
+        }
+        
+        struct Employee
+        {
+            public int Id;
+            public string FirstName;
+            public string LastName;
+
+
+        }
+    }
+}
