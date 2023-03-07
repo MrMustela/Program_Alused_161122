a955d86f04d2c34e1301850b18eeaface8b5b52f
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-03-07 14:55:21 +0200

    7.märts
    07.03.23 - LINQ

---
+++ 7märts/7märts/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _7märts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ");
+            WhereLINQ();
+            Console.WriteLine("----------------------------------");
+            ThenByLINQ();
+            Console.WriteLine("----------------------------------");
+            ThenByDescendingLINQ();
+            Console.WriteLine("----------------------------------");
+            ToLookUpLINQ();
+            Console.WriteLine("----------------------------------");
+            JoinLINQ();
+        }
+        public static void WhereLINQ()
+        {
+            var peopleAge = PeopleList.peoples.Where(x => x.Age > 20 && x.Age < 23);
+
+            foreach (var item in peopleAge)
+            {
+                Console.WriteLine(item.Name);
+                
+            }
+        }
+        
+        public static void ThenByLINQ()
+        {
+            Console.WriteLine("ThenBy järgi reastamine");
+
+            var thenByResult = PeopleList.peoples
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Age);
+
+            foreach (var people in thenByResult)
+            {
+                Console.WriteLine(people.Name + " " + people.Age);
+            }
+        }
+
+        public static void ThenByDescendingLINQ()
+        {
+            Console.WriteLine("ThenByDescending");
+
+            var thenByDescending = PeopleList.peoples
+                .OrderBy(x => x.Name)
+                .ThenByDescending(x => x.Age);
+
+            foreach (var people in thenByDescending)
+            {
+                Console.WriteLine(people.Name + " " + people.Age);
+            }
+        }
+        public static void ToLookUpLINQ()
+        {
+            Console.WriteLine("ToLookUp järgi reastamine");
+
+            var toLookUp = PeopleList.peoples
+                .ToLookup(x => x.Age);
+
+            foreach (var people in toLookUp)
+            {
+                Console.WriteLine("Age group " + people.Key);
+
+                foreach (People p in people)
+                {
+                    Console.WriteLine("Person name " + p.Name);
+                }
+            }
+        }
+
+        public static void JoinLINQ()
+        {
+            Console.WriteLine("InnerJoin in LINQ");
+
+            var InnerJoin = PeopleList.peoples
+                .Join(GenderList.genderList,
+                humans => humans.GenderId,
+                gender => gender.Id,
+                (humans, gender) => new
+                {
+                    Name = humans.Name,
+                    Sex = gender.Sex
+                });
+
+            foreach (var people in InnerJoin)
+            {
+                Console.WriteLine(people.Name + " " + people.Sex);
+            }
+        }
+    }
+}
