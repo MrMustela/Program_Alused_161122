528ec64c1e62a7921eefa44d142e936def1a1de2
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-03-08 10:12:29 +0200

    test

---
+++ 7märts/7märts/PeopleList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _7märts
+{
+    public class PeopleList
+    {
+        public static readonly List<People> peoples = new List<People>
+        {
+            new People()
+            {
+                Id = 1,
+                Name = "Moona",
+                Age = 11,
+                GenderId = Guid.Parse("4db6ecb3-6f3e-4087-ada4-91bebe48a132")
+            },
+            new People()
+            {
+                Id = 2,
+                Name = "Poona",
+                Age = 21,
+                GenderId = Guid.Parse("4db6ecb3-6f3e-4087-ada4-91bebe48a132")
+            },
+            new People()
+            {
+                Id = 3,
+                Name = "Tooma",
+                Age = 24,
+                GenderId = Guid.Parse("4d894ab3-815b-42e6-805d-6145d671d0a3")
+            },
+            new People()
+            {
+                Id = 4,
+                Name = "Soona",
+                Age = 42,
+                GenderId = Guid.Parse("4db6ecb3-6f3e-4087-ada4-91bebe48a132")
+            },
+            new People()
+            {
+                Id = 5,
+                Name = "Mari",
+                Age = 17,
+                GenderId = Guid.Parse("4db6ecb3-6f3e-4087-ada4-91bebe48a132")
+            },
+            new People()
+            {
+                Id = 6,
+                Name = "Mari",
+                Age = 21,
+                GenderId = Guid.Parse("4db6ecb3-6f3e-4087-ada4-91bebe48a132")
+            },
+            new People()
+            {
+                Id = 7,
+                Name = "Bill",
+                Age = 21,
+                GenderId = Guid.Parse("4d894ab3-815b-42e6-805d-6145d671d0a3")
+            },
+        };
+    }
+}
---
+++ 7märts/7märts/Program.cs
@@ -0,0 +1,148 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _7märts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ");
+            WhereLINQ();
+            Console.WriteLine("----------------------------------");
+            ThenByLINQ();
+            Console.WriteLine("----------------------------------");
+            ThenByDescendingLINQ();
+            Console.WriteLine("----------------------------------");
+            ToLookUpLINQ();
+            Console.WriteLine("----------------------------------");
+            JoinLINQ();
+            Console.WriteLine("----------------------------------");
+            GroupJoinLINQ();
+            Console.WriteLine("----------------------------------");
+            //SelectLINQ();
+            Console.WriteLine("----------------------------------");
+            AllAndAnyLINQ();
+        }
+        public static void WhereLINQ()
+        {
+            var peopleAge = PeopleList.peoples.Where(x => x.Age > 20 && x.Age < 23);
+
+            foreach (var item in peopleAge)
+            {
+                Console.WriteLine(item.Name);
+
+            }
+        }
+
+        public static void ThenByLINQ()
+        {
+            Console.WriteLine("ThenBy järgi reastamine");
+
+            var thenByResult = PeopleList.peoples
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Age);
+
+            foreach (var people in thenByResult)
+            {
+                Console.WriteLine(people.Name + " " + people.Age);
+            }
+        }
+
+        public static void ThenByDescendingLINQ()
+        {
+            Console.WriteLine("ThenByDescending");
+
+            var thenByDescending = PeopleList.peoples
+                .OrderBy(x => x.Name)
+                .ThenByDescending(x => x.Age);
+
+            foreach (var people in thenByDescending)
+            {
+                Console.WriteLine(people.Name + " " + people.Age);
+            }
+        }
+        public static void ToLookUpLINQ()
+        {
+            Console.WriteLine("ToLookUp järgi reastamine");
+
+            var toLookUp = PeopleList.peoples
+                .ToLookup(x => x.Age);
+
+            foreach (var people in toLookUp)
+            {
+                Console.WriteLine("Age group " + people.Key);
+
+                foreach (People p in people)
+                {
+                    Console.WriteLine("Person name " + p.Name);
+                }
+            }
+        }
+
+        public static void JoinLINQ()
+        {
+            Console.WriteLine("InnerJoin in LINQ");
+
+            var InnerJoin = PeopleList.peoples
+                .Join(GenderList.genderList,
+                humans => humans.GenderId,
+                gender => gender.Id,
+                (humans, gender) => new
+                {
+                    Name = humans.Name,
+                    Sex = gender.Sex
+                });
+
+            foreach (var people in InnerJoin)
+            {
+                Console.WriteLine(people.Name + " " + people.Sex);
+            }
+        }
+
+        public static void GroupJoinLINQ()
+        {
+            var groupJoin = GenderList.genderList
+                .GroupJoin(PeopleList.peoples,
+                p => p.Id,
+                g => g.GenderId,
+                (p, peopleGroup) => new
+                {
+                    humans = peopleGroup,
+                    GenderFullName = p.Sex
+                });
+
+            foreach (var people in groupJoin)
+            {
+                Console.WriteLine(people.GenderFullName);
+
+                foreach (var name in people.humans)
+                {
+                    Console.WriteLine(name.Name);
+                }
+            }
+        }
+
+      //  public static void SelectLINQ()
+      //  {
+      //      Console.WriteLine("Select in LINQ");
+
+      //      var SelectLINQ = PeopleList.peoples
+      //          .Select(p => p.Name);
+
+      //      foreach (var peopleName in SelectLINQ)
+      //      {
+      //          Console.WriteLine(peopleName.name + " " + peopleName.age);
+      //      }
+      //  }
+        
+        public static void AllAndAnyLINQ()
+        {
+            Console.WriteLine("All LINQ");
+
+            bool areALLPeopleTeenagers = PeopleList.peoples
+                .All(x => x.Age > 10);
+
+            Console.WriteLine(areALLPeopleTeenagers);
+        }
+    }
+}
