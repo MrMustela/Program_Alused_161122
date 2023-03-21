f2067661488cfb969c97f2c96bd32062971820af
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-03-21 14:57:45 +0200

    21.03.23 - fail3
    Faili lugemine

---
+++ 21märts_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _21märts_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faili lugemine");
+
+            string line;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:/Users/opilane/Desktop/luuletus.txt"))
+                {
+                    int count = 0;
+
+                    while (sr.EndOfStream == false)
+                    {
+                        ++count;
+                        line = sr.ReadLine();
+                        int charLine = line.Length;
+                        Console.WriteLine(count + " " + line + " " + charLine);
+                    }
+                    sr.Close();
+                }
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Faili ei saa lugeda");
+            }
+        }
+    }
+}
