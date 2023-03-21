422dd4439b62cff720500e9adfc22025124621c5
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-03-21 13:25:48 +0200

    21.03.23
    faili tegemine

---
+++ 21märts/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _21märts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta faili läbi konsooli");
+
+            string filePath = @"C:/Users/Opilane/Desktop/WriteToFile.txt";
+            string inputText = Console.ReadLine();
+            
+            File.WriteAllText(filePath, inputText);
+
+        }
+    }
+}
