c34e792daec962c9665d12a30c44ec398af47ea1
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-03-28 14:04:18 +0300

    28.märts - iseseisev
    faili harjutus

---
+++ 28märts/28märts/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _28märts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faili harjutused");
+            Console.WriteLine("\nSisesta õige fail URL lõpuni");
+            string url = Console.ReadLine();
+
+            string filePath = @"C:/Users/Opilane/Desktop/" + url;
+
+            if (url == "WriteToFile.txt")
+            {
+                Console.WriteLine("Correct!");
+
+                Console.WriteLine("Sisesta mingi text siia");
+                string inputText = Console.ReadLine();
+                File.WriteAllText(filePath, inputText);
+
+
+            }
+
+            else
+                Console.WriteLine("ERROR!");
+
+            
+        }
+    }
+}
