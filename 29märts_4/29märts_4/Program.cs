53fd3272c1d4fb920480a2fd5d4dba62b71284f9
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-03-29 11:39:43 +0300

    Motivatsioon ülesanne 29märts
    29.03.23

---
+++ 29märts_4/29märts_4/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _29märts_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Enter VIN Code");
+
+            int vinCode = Convert.ToInt32(Console.ReadLine());
+
+            Machine machine = new Machine();
+            machine.SetVINNumber(vinCode);
+
+            Console.WriteLine("Vin code is: " + machine.GetVINCode());
+        }
+    }
+
+    class Car
+    {
+        public void SetVINNumber(int v)
+        {
+            VIN = v;
+        }
+        protected int VIN;
+    }
+
+    class Machine : Car
+    {
+        public int GetVINCode()
+        {
+            return VIN;
+        }
+    }
+    
+}
