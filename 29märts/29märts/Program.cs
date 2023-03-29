9e76ffc7da1b9968194754906458aa6a66990e66
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-03-29 10:06:55 +0300

    29.03.23
    Mapping

---
+++ 29märts/29märts/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _29märts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mapper");
+            Car car1 = new Car();
+            car1.Id = 1;
+            car1.Color = "Black";
+            car1.CarType = "Toyota";
+            car1.PlateNumber = "123ABC";
+
+            CarDto dto = new CarDto();
+            dto.Id = car1.Id;
+            dto.Color = car1.Color;
+            dto.CarType = car1.CarType;
+            dto.PlateNumber = car1.PlateNumber;
+
+            Console.WriteLine("Car: " + dto.Id + ", " + dto.CarType);
+       
+        }
+
+    }
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public string CarType { get; set; }
+        public string PlateNumber { get; set; }
+    }
+
+    public class CarDto
+    {
+       public int Id { get; set; }
+       public string Color { get; set; }
+       public string CarType { get; set; }
+       public string PlateNumber { get; set; }
+    }
+}
+    
+
+    
+    
+
+    
