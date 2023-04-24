9c39830901e2175726f6ffa6b734a7850f6cc2a9
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-04-24 11:22:13 +0300

    24.04.23
    Hello, World!

---
+++ 24.aprill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace _24.aprill
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                HelloWorld();
+            }
+        }
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        
+    }
+}
