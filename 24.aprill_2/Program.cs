e2799ca4cd89cd25e740a6a3243b3dba71480576
MrMustela <118337312+MrMustela@users.noreply.github.com>
2023-04-24 11:23:26 +0300

    24.04.2023
    Ises. Vabatahtlik. Ruudu ja püramiidi tegemine.

---
+++ 24.aprill_2/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace _24.aprill_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Experiment. Ises. ; Vabatahtlik (24.04.2023)
+            Console.WriteLine("Ruudu tegemine");
+            Square();
+            Console.WriteLine("\n------------");
+            Square2();
+            Console.WriteLine("\n------------");
+            Pyramide();
+            static void Square()
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < 5; j++)
+                    {
+                        Console.Write("* ");
+                    }
+                }
+            }
+            static void Square2()
+            {
+                int sum = 5;
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < sum; j++)
+                    {
+                        Console.Write("* ");
+                        if (i == 1 && j == 0)
+                        {
+                            for (int k = 0; k < 0; k++)
+                            Console.Write("ok");
+                        }
+
+                        else if (i == 2 && j == 0)
+                        {
+                            for (int k = 0; k < 6; k++)
+                                Console.Write(" ");
+                        }
+                        
+                        else if (i == 3 && j == 0)
+                        {
+                            for (int k = 0; k < 6; k++)
+                                Console.Write(" ");
+                        }
+                    }
+                }
+            }
+
+            static void Pyramide()
+            {
+                int tarn = 0;
+                for (int i = 0; i < 11; i++)
+                {
+                    Console.WriteLine();
+                    tarn++;
+                    for (int j = 0; j < tarn; j++)
+                    {
+                        Console.Write("* ");
+                    }
+                }
+            }
+        }
+    }
+}
